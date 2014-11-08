743f16a47cfa81e524361bc3c498d06a97f2720e
Takashi Kanemoto <kanemoto.takashi@gmail.com>
2014-11-09 01:47:27 +0900

    add some unit tests

--- tests/Provider/TwigMessageServiceProviderTest.php
+++ tests/Provider/TwigMessageServiceProviderTest.php
@@ -1,18 +1,32 @@
 <?php
 namespace Quartet\Silex\Provider;
 
+use Quartet\Silex\Service\ImageEmbedder\Embedder;
+use Quartet\Silex\Twig\Extension\TwigMessageExtension;
+use Silex\Application;
+
 class TwigMessageServiceProviderTest extends \PHPUnit_Framework_TestCase
 {
-    /** @var TwigMessageServiceProvider */
-    protected $provider;
+    /** @var TwigMessageExtension */
+    private $extension;
 
     protected function setUp()
     {
+        $this->extension = new TwigMessageExtension(new Embedder());
     }
 
-    public function test()
+    public function test_register()
     {
-        // todo: to test provider with some mocks.
-        $this->assertTrue(true);
+        $app = new Application();
+        $app->register(new TwigMessageServiceProvider());
+
+        $this->assertInstanceOf('Quartet\Silex\Service\TwigMessageService', $app['twig_message']);
+        $this->assertInstanceOf('Twig_Environment', $app['twig']);
+        $this->assertInstanceOf('Swift_Mailer', $app['mailer']);
+
+        $extensions = $app['twig']->getExtensions();
+        $this->assertContains($this->extension->getName(), array_keys($extensions));
+
+        $this->assertInstanceOf(get_class($this->extension), $extensions[$this->extension->getName()]);
     }
 }
--- tests/Service/TwigMessageServiceTest.php
+++ tests/Service/TwigMessageServiceTest.php
@@ -1,6 +1,9 @@
 <?php
 namespace Quartet\Silex\Service;
 
+use Quartet\Silex\Exception\RuntimeException;
+use Quartet\Silex\Service\ImageEmbedder\Embedder;
+
 class TwigMessageServiceTest extends \PHPUnit_Framework_TestCase
 {
     /** @var TwigMessageService */
@@ -8,11 +11,33 @@ class TwigMessageServiceTest extends \PHPUnit_Framework_TestCase
 
     protected function setUp()
     {
+        $this->service = new TwigMessageService(new \Twig_Environment(), new Embedder());
     }
 
-    public function test()
+    public function test_setInlineStyle()
     {
-        // todo: to test service with some mocks.
-        $this->assertTrue(true);
+        $body = '<p>test</p>';
+        $style = 'p { color: #fff; }';
+        $expectedBody = '<p style="color: #fff;">test</p>';
+
+        $message = new \Swift_Message();
+
+        $message->setBody($body, 'text/html');
+        $this->service->setInlineStyle($message, $style);
+
+        $this->assertContains($expectedBody, $message->getBody());
+    }
+
+    public function test_setInlineStyle_error_for_plain_text()
+    {
+        $body = '<p>test</p>';
+        $style = 'p { color: #fff; }';
+
+        $message = new \Swift_Message();
+
+        $message->setBody($body);
+
+        $this->setExpectedException(get_class(new RuntimeException()));
+        $this->service->setInlineStyle($message, $style);
     }
 }
